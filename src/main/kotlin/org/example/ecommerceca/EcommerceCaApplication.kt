7cf5e3dd2ffd1be892a848c2b17dc6a493c249c8
Marcos Medina <mimc.software@gmail.com>
2026-05-29 23:01:53 +0100

    initialize ecommerceCA project with Gradle, Spring Boot, and MySQL configuration

---
+++ src/main/kotlin/org/example/ecommerceca/EcommerceCaApplication.kt
@@ -0,0 +1,11 @@
+package org.example.ecommerceca
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceCaApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcommerceCaApplication>(*args)
+}
