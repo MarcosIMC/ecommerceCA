a33361c37060ff96946ec407b85fe0db21f66c21
Marcos Medina <mimc.software@gmail.com>
2026-06-18 19:45:45 +0100

    Decouple the use case from Spring Boot by removing the annotation and creating a class in which you map the annotations to the use cases

--- src/main/kotlin/org/example/ecommerceca/item/application/usecase/ListItemUseCase.kt
+++ src/main/kotlin/org/example/ecommerceca/item/application/usecase/ListItemUseCase.kt
@@ -2,11 +2,8 @@ package org.example.ecommerceca.item.application.usecase
 
 import org.example.ecommerceca.item.domain.ItemRepository
 import org.example.ecommerceca.item.domain.model.Item
-import org.example.ecommerceca.item.infraestructure.controller.POJOs.ItemResponseDTO
-import org.springframework.stereotype.Service
 import kotlin.uuid.ExperimentalUuidApi
 
-@Service
 class ListItemUseCase(private val repository: ItemRepository) {
     @OptIn(ExperimentalUuidApi::class)
     fun execute(): List<Item> {
---
+++ src/main/kotlin/org/example/ecommerceca/item/infraestructure/config/ItemUseCaseConfig.kt
@@ -0,0 +1,12 @@
+package org.example.ecommerceca.item.infraestructure.config
+
+import org.example.ecommerceca.item.application.usecase.ListItemUseCase
+import org.example.ecommerceca.item.domain.ItemRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class ItemUseCaseConfig {
+    @Bean
+    open fun listItemUseCase(repository: ItemRepository) = ListItemUseCase(repository)
+}
